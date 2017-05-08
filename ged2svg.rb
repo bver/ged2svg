aa481bd337ff162b570f4aa87b134000d92cb691
Pavel Suchmann <pavel@suchmann.cz>
2017-05-08 16:17:21 +0200

    rendering

--- ged2svg.rb
+++ ged2svg.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
 
 require_relative 'parse_ged'
+require_relative 'render_tree'
 
 abort "Usage:\n #$0 file.ged" unless ARGV.size == 1
 
-people = parse_ged ARGV.first
+#people = parse_ged ARGV.first
 #people.each_value {|p| puts p.birth; puts p.death }
-p people
+#p people
+
+people = { 'root' => Person.new('Wacslav', 'Ssuchmann', 'M', '1 JAN 1900', '1 JAN 2000') }
+puts render_tree(people, 'root')
+
 
 
 
--- person.rb
+++ person.rb
@@ -23,6 +23,14 @@ class Person
     @families[spouse_ref]
   end
 
+  def livespan
+    if @birth.nil?
+      @death.nil? ? '' : "+ #{@death}"
+    else  
+      @death.nil? ? "* #{@birth}" : "#{@birth} - #{@death}" 
+    end  
+  end
+
   @@months = { 'JAN'=>1, 'FEB'=>2, 'MAR'=>3, 'APR'=>4, 'MAY'=>5, 'JUN'=>6, 
                'JUL'=>7, 'AUG'=>8, 'SEP'=>9, 'OCT'=>10, 'NOV'=>11, 'DEC'=>12 }
   @@pref = { 'ABT' => '~', 'EST' => '~', 'BEF' => '<', 'AFT' => '>' }
